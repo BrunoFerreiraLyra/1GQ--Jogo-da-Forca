c9004e799e00702faf07a79188c1f2459b98718b
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-03-31 20:51:45 -0300

    load

--- jogo-forca/app/page.js
+++ jogo-forca/app/page.js
@@ -6,24 +6,13 @@ import { palavras } from './utils/palavras';
 import styles from './page.module.css';
 
 export default function JogoDaForca() {
-  
- 
   const [palavraAtual, setPalavraAtual] = useState("");
-  
   const [letrasTentadas, setLetrasTentadas] = useState([]);
-  
   const [erros, setErros] = useState(0);
-  
   const [statusDoJogo, setStatusDoJogo] = useState("jogando"); 
 
-
-
-
- 
   const iniciarNovoJogo = () => {
-
     const indiceAleatorio = Math.floor(Math.random() * palavras.length);
-    
     setPalavraAtual(palavras[indiceAleatorio]);
     setLetrasTentadas([]);
     setErros(0);
@@ -33,28 +22,29 @@ export default function JogoDaForca() {
   useEffect(() => {
     iniciarNovoJogo();
   }, []);
-}
 
-  // Substitua apenas o 'return' do seu app/page.js por este:
+ 
+  if (!palavraAtual) {
+    return (
+      <main className={styles.main}>
+        <p>Carregando jogo...</p>
+      </main>
+    );
+  }
+
 
   return (
     <main className={styles.main}>
       <h1>Jogo da Forca - Países</h1>
       
-      {/* Caixa de Debug (Vamos deixar aqui por enquanto para facilitar os testes, 
-        depois nós apagamos quando o jogo estiver pronto) 
-      */}
       <div style={{ marginTop: '20px', padding: '10px', backgroundColor: '#e0e0e0' }}>
         <p><strong>(Debug) País Sorteado:</strong> {palavraAtual}</p>
         <p><strong>(Debug) Erros Atuais:</strong> {erros}</p>
       </div>
 
-      {/* --- ETAPA 3: A PALAVRA OCULTA --- */}
       <div className={styles.palavraContainer}>
         {palavraAtual.split('').map((letra, index) => {
-          // Verifica se a letra atual da iteração já foi clicada pelo usuário
           const revelada = letrasTentadas.includes(letra);
-          
           return (
             <span key={index} className={styles.letraCaixa}>
               {revelada ? letra : ""}
@@ -62,6 +52,6 @@ export default function JogoDaForca() {
           );
         })}
       </div>
-
     </main>
   );
+}
