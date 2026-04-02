7b6ae1052ea27d67c21ec55e3dade35759818043
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-04-02 19:31:31 -0300

    dsssssssssadadwere

--- jogo-forca/app/page.js
+++ jogo-forca/app/page.js
@@ -2,11 +2,11 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import { palavras } from './utils/palavras';
+import { palavras, codigosBandeiras } from './utils/palavras';
 import styles from './page.module.css';
-import { palavras , codigosBandeiras } from './utils/palavras';
 
 const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
 
 export default function JogoDaForca() {
   const [palavraAtual, setPalavraAtual] = useState("");
